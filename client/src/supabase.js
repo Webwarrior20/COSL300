4cb55ca7acdbad26121ea422e96cb9f06d42c671
vedantprajapati <vuprajapati20@gmail.com>
2026-05-27 22:29:57 -0600

    small updates

--- client/src/supabase.js
+++ client/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://quvqwamgicwhnwevbyzj.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_4GWRJFGdEgNhBmCb2aAhRQ_4oqmKN-V";
+const SUPABASE_URL = "https://kcbiuaskauyqkmsrbbcf.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_rWPqR5KxyJRx00AMK3YImg_exUARu0q";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
